951ddb643989f8464a9f452c16f7a84b7b033e6a
Sergey Voloshin <sv@anahoret.com>
2015-08-31 15:05:05 +0300

    Rework with oop patterns.

--- index.js
+++ index.js
@@ -2,75 +2,76 @@
 var moment = require('moment');
 var _ = require('underscore');
 
-var browserStorage = (function () {
-  "use strict";
+require('./SimpleInheritance');
 
-  var module = {};
-  var opts = {
-    name: 'SimpleBrowserStorage',
-    existenceTime: 10 // in minutes;
-  };
+var Storage = Class.extend({
+  init: function(opts){
+    this.name = opts.name || 'SimpleBrowserStorage';
+    this.expiresTime = opts.expiresTime || 10;
+    this.expired = false;
 
-  var hasStorage = (function () {
-    try {
-      var mod = 'test';
-      localStorage.setItem(mod, mod);
-      localStorage.removeItem(mod);
-      return true;
-    } catch (exception) {
-      return false;
-    }
-  }());
+    var state   = this.getState() || {},
+        expires = state.expires,
+        willExpire = {expires: moment()._d};
 
-  if (hasStorage) {
-    module.setState = function (value) {
-      localStorage.setItem(opts.name, JSON.stringify(value));
-    };
-
-    module.getState = function () {
-      return JSON.parse(localStorage.getItem(opts.name));
-    };
-  } else {
-    module.setState = function (value) {
-      var d = new Date();
-      var days100 = 100 * 24 * 60 * 60 * 1000;
-      d.setTime(d.getTime() + days100);
-      var expires = "expires=" + d.toUTCString();
-      var cookie = [opts.name, '=', escape(JSON.stringify(value)), ';', expires].join('');
-      document.cookie = cookie;
-    };
-    module.getState = function () {
-      var result = document.cookie.match(new RegExp(opts.name + '=([^;]+)'));
-      if (result) {
-        result = JSON.parse(unescape(result[1]));
-      }
-      return result;
-    };
-  }
-
-  module.init = function(customOpts) {
-    opts = _.defaults(opts, customOpts);
-    var state   = this.getState();
-    var expires = state.expires;
-    var willExpire = {expires: moment()._d}
-    if (!expires || moment().diff(expires, 'minutes') > opts.existenceTime) {
+    if (!expires || moment().diff(expires, 'minutes') > this.expiresTime) {
       this.setState(willExpire);
       this.expired = true;
     } else {
-      this.setState(_.defaults(state, willExpire));
+      this.setState(state);
       this.expired = false;
     }
-  };
-
-  module.stateIsExpired = function(){
+  },
+  stateIsExpired: function() {
     return this.expired;
   }
+});
 
-  if (module.getState() === null) {
-    module.setState({});
+var LocalStorage = Storage.extend({
+  init: function(opts) {
+    this._super(opts);
+  },
+  setState: function(value) {
+    localStorage.setItem(this.name, JSON.stringify(value));
+  },
+  getState: function() {
+    return JSON.parse(localStorage.getItem(this.name));
   }
+});
 
-  return module;
-})();
+var CookieStorage = Storage.extend({
+  init: function(opts) {
+    this._super(opts);
+  },
+  setState: function(value) {
+    var d = new Date();
+    var days100 = 100 * 24 * 60 * 60 * 1000;
+    d.setTime(d.getTime() + days100);
+    var expires = "expires=" + d.toUTCString();
+    var cookie = [this.name, '=', escape(JSON.stringify(value)), ';', expires].join('');
+    document.cookie = cookie;
+  },
+  getState: function() {
+    var result = document.cookie.match(new RegExp(this.name + '=([^;]+)'));
+    return result ? JSON.parse(unescape(result[1])) : result;
+  }
+});
 
-module.exports = browserStorage;
+var hasLocalStorage = (function() {
+  try {
+    var mod = 'test';
+    localStorage.setItem(mod, mod);
+    localStorage.removeItem(mod);
+    return true;
+  } catch (exception) {
+    return false;
+  }
+}());
+
+module.exports = function(opts) {
+  if (hasLocalStorage) {
+    return new LocalStorage(opts);
+  } else {
+    return new CookieStorage(opts);
+  }
+};
